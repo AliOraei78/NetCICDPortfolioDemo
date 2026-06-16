016953cb9090efbeff6791e3cc49fe90f11ff8cb
AliJenabi78 <a.jenabi78@gmail.com>
2026-06-16 15:12:52 +0330

    Day 4: Add xUnit test project + Update workflow with Restore, Build and Test

---
+++ NetCICDPortfolioDemo.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using NetCICDPortfolioDemo.Controllers;
+
+namespace NetCICDPortfolioDemo.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_Returns_WeatherForecasts()
+        {
+            // Arrange
+            var controller = new WeatherForecastController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
